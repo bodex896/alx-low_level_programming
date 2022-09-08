cecafdc4aec9c59d4be9bdd3ac87d80a758b631e
bodex896 <bodex896.alabiolabode6@gmail.com>
2022-09-08 16:55:53 -0700

    my 4th  commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,2 @@
+#!/bin/bash
+#include <stdio.h>\n/**\n* main - Entry point\n*\n* Return: always 0\n*/\nint main(void)\n{\nputs("\"Programming is like building a multilingual puzzle");\nreturn (0);\n}
