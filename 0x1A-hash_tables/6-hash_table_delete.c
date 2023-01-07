7b7dc682397d66f90d4d95202e242fab1a53d4db
bodex896 <bodex896.alabiolabode6@gmail.com>
2023-01-07 00:21:43 -0800

    my  commit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,26 +1,30 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_delete - deletes and frees hash table
- * @ht: hash table to beat
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned int i;
-	hash_node_t *freer, *tmp;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		freer = ht->array[i];
-		while (freer)
+		if (ht->array[i] != NULL)
 		{
-			tmp = freer->next;
-			free(freer->key);
-			free(freer->value);
-			free(freer);
-			freer = tmp;
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
 		}
 	}
-	free(ht->array);
-	free(ht);
-
+	free(head->array);
+	free(head);
 }
