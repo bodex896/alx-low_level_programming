9a6e4cf4d3a4b07ba68ae7f2d9b6b61bcc9f75c8
bodex896 <bodex896.alabiolabode6@gmail.com>
2023-01-07 00:20:36 -0800

    my  commit

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,59 +1,38 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_print - prints hash table
- * @ht: hash table to print
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned int i = 0;
-	unsigned int lastKey;
-	hash_node_t *printer;
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char comma_flag = 0;
 
 	if (ht == NULL)
 		return;
-	lastKey = ht->size - 1;
-	while (!ht->array[lastKey] && lastKey != 0)
-		lastKey--;
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		printer = ht->array[i];
-		while (printer)
+		if (ht->array[i] != NULL)
 		{
-			printf("'%s': '%s'", printer->key, printer->value);
-			if (i != lastKey)
+			if (comma_flag == 1)
 				printf(", ");
-			printer = printer->next;
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
 		}
 	}
-	printf("}");
-	printf("\n");
-}
-6-hash_table_delete.c
-#include "hash_tables.h"
-/**
- * hash_table_delete - deletes and frees hash table
- * @ht: hash table to beat
- */
-void hash_table_delete(hash_table_t *ht)
-{
-	unsigned int i;
-	hash_node_t *freer, *tmp;
-
-	for (i = 0; i < ht->size; i++)
-	{
-		freer = ht->array[i];
-		while (freer)
-		{
-			tmp = freer->next;
-			free(freer->key);
-			free(freer->value);
-			free(freer);
-			freer = tmp;
-		}
-	}
-	free(ht->array);
-	free(ht);
-
+	printf("}\n");
 }
