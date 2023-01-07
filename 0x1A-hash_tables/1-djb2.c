7564883e3a2e6db8b8a453a7b3a841a669eac1df
bodex896 <bodex896.alabiolabode6@gmail.com>
2023-01-07 00:12:58 -0800

    my  commit

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,8 +1,9 @@
 #include "hash_tables.h"
+
 /**
- * hash_djb2 - a very important algorithm
- * @str: takes a string and hashes it out
- * Return: really big integer
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ * Return: The calculated hash.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -10,22 +11,8 @@ unsigned long int hash_djb2(const unsigned char *str)
 	int c;
 
 	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
-}
-2-key_index.c
-#include "hash_tables.h"
-/**
- * key_index - returns index of hashtable value
- * @key: key to test
- * @size: size of hash table
- * Return: modulo of hashed key to size
- */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
 
-	return (hash_djb2(key) % size);
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	return (hash);
 }
